0eeead642c2d82d374be051e557ca4adb26bb209
Jeffrey Davis <jeffreydanieldavis@gmail.com>
2017-06-27 17:45:35 -0400

    Started using pug templating engine and koa-route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,43 @@
 import koa from 'koa';
+import route from 'koa-route';
+import views from 'koa-views';
 const app = new koa();
 
-app.use(ctx => {
-  ctx.body = 'Hello Emelia the bozo!';
-});
+app.use(views(`${__dirname}/server/views`, { extension: 'pug' }))
+
+const db = {
+  something1: { name: 'shirt', price: 9.99 },
+  something2: { name: 'sweater', price: 19.99 },
+  something3: { name: 'pants', price: 40 },
+};
+
+const products = {
+  list: (ctx) => {
+    const names = Object.keys(db);
+    ctx.body =  `products: ${names.join(', ')}`;
+  },
+
+  show: (ctx, name) => {
+    const product = db[name];
+    if (!product) {
+      return ctx.throw('cannot find that product', 404);
+    }
+    ctx.body = `${product.name} is $${product.price.toFixed(2)}`;
+  },
+};
+
+app.use(route.get('/products', products.list));
+app.use(route.get('/products/:name', products.show));
+app.use(route.get('/about', (ctx) => {
+  ctx.body = 'about this site';
+}));
+
+app.use(async function (ctx) {
+  ctx.state = {
+    youAreUsingPug: true,
+  };
+  
+  await ctx.render('home')
+})
 
 app.listen(3000);
